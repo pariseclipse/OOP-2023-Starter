de6a23e4bfe496baa7831c3aa39f0fc675dcdac1
pariseclipse <104399068+pariseclipse@users.noreply.github.com>
2023-04-26 17:08:49 +0100

    Adding public void DANI and public void fileLoading

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,7 +6,33 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	testing
+	private ArrayList<Text> model;
+	private PApplet parent;
+
+	public DANI(PApplet parent) {
+		model = new ArrayList<Text>();
+		this.parent = parent;
+	}
+
+	public void fileLoading(String DANI) {
+		String[] lines = parent.loadStrings(DANI);
+		for (int i = 0; i < lines.length; i++) {
+			String[] texts = parent.split(lines[i], ' ');
+			for (int j = 0; j < texts.length; j++) {
+				String w = texts[j].toLowerCase().replaceAll("[^\\w\\s]","");
+				if (!findText(w)) {
+				  Text newText = new Text(w);
+				  model.add(newText);
+				}
+				if (j < texts.length - 1) {
+				  String f = texts[j + 1].toLowerCase().replaceAll("[^\\w\\s]","");
+				  Text text = getText(w);
+				  text.addFollow(f);
+				}
+			  }
+			}
+		  }
+		
 
 	public void settings() {
 		size(1000, 1000);
